a2c4cfe9da11aec89c7c11112c4b635524cb8958
Stanley Adeka <adekastanley2@gmail.com>
2026-09-04 14:36:53 +0100

    under construction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Hero } from '../components/Hero'
-import { About } from '../components/About'
-import { Impact } from '../components/Impact'
-import { HealthAgenda } from '../components/HealthAgenda'
-import { MediaHub } from '../components/MediaHub'
-import { Volunteer } from '../components/Volunteer'
+import { UnderConstruction } from '../components/UnderConstruction'
 
 export const Route = createFileRoute('/')({
-    component: RouteComponent,
+    component: UnderConstruction,
 })
 
-function RouteComponent() {
-    return (
-        <div className="w-full max-w-full overflow-x-hidden">
-            <Hero />
-            <About />
-            <Impact />
-            <HealthAgenda />
-            <MediaHub />
-            <Volunteer />
-        </div>
-    )
-}
